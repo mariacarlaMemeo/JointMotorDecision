04630b51a78eb9b878536db3384574ef2204a95f
mariacarlaMemeo <mariacarla.memeo@iit.it>
2023-03-06 17:21:57 +0100

    edit - start and end of the trial to consider for calculating the variables

--- analyses/movement_var.m
+++ analyses/movement_var.m
@@ -7,12 +7,14 @@ samp       = 1:sMarkers{t}.info.nSamples;
 index      = sMarkers{t}.markers.([model_name '_index']);
 ulna       = sMarkers{t}.markers.([model_name '_ulna']);
 
+const = 0;%%remove last 'const' frames for index values
+
 %Calc kin vars - temporal variables
 %average speed
-va_index = mean(index.Vm(startFrame:endFrame));% 
+va_index = mean(index.Vm(startFrame:endFrame-const));% 
 va_ulna  = mean(ulna.Vm(startFrame:endFrame));% 
 %average acceleration
-aa_index = mean(index.Am(startFrame:endFrame));% 
+aa_index = mean(index.Am(startFrame:endFrame-const));% 
 aa_ulna  = mean(ulna.Am(startFrame:endFrame));% 
 %average jerk
 ja_index = index.Jmean;
@@ -24,17 +26,17 @@ tulna  = [va_ulna aa_ulna ja_ulna];
 
 %Calc kin vars - spatial variables
 %peak hight (z coord)
-pz_index = max(index.xyzf(:,3));
-pz_ulna  = max(ulna.xyzf(:,3));
+pz_index = max(index.xyzf((startFrame:endFrame-const),3));
+pz_ulna  = max(ulna.xyzf((startFrame:endFrame),3));
 %minimum hight (z coord)
-mz_index = min(index.xyzf(:,3));
-mz_ulna  = min(ulna.xyzf(:,3));
+mz_index = min(index.xyzf((startFrame:endFrame-const),3));
+mz_ulna  = min(ulna.xyzf((startFrame:endFrame),3));
 %mean hight (z coord)
-za_index = mean(index.xyzf(:,3));
-za_ulna  = mean(ulna.xyzf(:,3));
+za_index = mean(index.xyzf((startFrame:endFrame-const),3));
+za_ulna  = mean(ulna.xyzf((startFrame:endFrame),3));
 %area of hight (z coord)
-az_index = trapz(index.xyzf(:,3));
-az_ulna  = trapz(ulna.xyzf(:,3));
+az_index = trapz(index.xyzf((startFrame:endFrame-const),3));
+az_ulna  = trapz(ulna.xyzf((startFrame:endFrame),3));
 
 %group spatial variables 
 sindex = [pz_index mz_index za_index az_index];
@@ -42,16 +44,16 @@ sulna  = [pz_ulna mz_ulna za_ulna az_ulna];
 
 %average deviation from straight line (queer spectrum)
 %calculate coeffs of the straight line - only for index marker
-x1    = index.xyzf(1,1);
-xend  = index.xyzf(end,1);
-y1    = index.xyzf(1,2);
-yend  = index.xyzf(end,2);
+x1    = index.xyzf(startFrame,1);
+xend  = index.xyzf(endFrame-const,1);
+y1    = index.xyzf(startFrame,2);
+yend  = index.xyzf(endFrame-const,2);
 coefs = polyfit([x1, xend], [y1, yend], 1);
-sline = coefs(1).*index.xyzf(startFrame:endFrame,1) + coefs(2);
+sline = coefs(1).*index.xyzf((startFrame:endFrame-const),1) + coefs(2);
 %calculate the area of the trajectory deviation (x,y) - neg values? 
-xa    = abs(index.xyzf(startFrame:endFrame,1));
-ya    = abs(index.xyzf(startFrame:endFrame,2));
-y     = index.xyzf(startFrame:endFrame,2);
+xa    = abs(index.xyzf((startFrame:endFrame-const),1));
+ya    = abs(index.xyzf((startFrame:endFrame-const),2));
+y     = index.xyzf((startFrame:endFrame-const),2);
 ard   = trapz(xa,abs(sline-y)); %trapz(abs(x),abs(y1-y2))
 
 d     = sqrt(sum(([xa,ya]-[xa,abs(sline)]).^2,2));
