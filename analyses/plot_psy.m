58ce111251f8ab428667e79918326f29a25163d4
smitzla <la.schmitz@uke.de>
2023-06-26 12:52:57 +0200

    edit glmfit (psychometric curves) for new data

--- analyses/plot_psy.m
+++ analyses/plot_psy.m
@@ -1,19 +1,23 @@
 function slope_pair=plot_psy(conSteps,y,plotSym,color,default,full,coll_calc)
 if full
-    %collect the slopes: [blue, yellow, coll, coll blue, colle yellow]
+    %collect the slopes: [blue, yellow, coll, coll blue, coll yellow,blue indiv. 1st dec, yellow indiv. 1st dec]
     slope_pair = zeros(1,width(y));
     for plt=1:width(y)
         bhat = glmfit(conSteps,[y(:,plt) ones(size(y(:,plt)))],'binomial','link','probit');
         d_mean = -bhat(1)/bhat(2);
         d_sd   = 1/bhat(2);
-        plot(conSteps, y(:,plt), plotSym{:,plt},'Color',color(plt,:));
-        hold on;
+        % plot the markers
+        if plt<=3 % if also indiv. coll. benefit should be plotted: plt<=5
+            plot(conSteps, y(:,plt), plotSym{:,plt},'MarkerSize',6,'LineWidth',1.5,'Color',color(plt,:));
+            hold on;
+        end
         C = 1.3 .* (min(conSteps) : 0.001 : max(conSteps));
         ps = cdf('norm',C,d_mean,d_sd);
+        % plot the lines
         if plt<=3
-            plot(C,ps,'-','LineWidth',2,'Color',color(plt,:));
-        else
-            plot(C,ps,'--','LineWidth',2,'Color',color(plt,:));
+            plot(C,ps,'-','LineWidth',1.5,'Color',color(plt,:));
+%         elseif plt>3 && plt<=5
+%             plot(C,ps,'--','LineWidth',2,'Color',color(plt,:));
         end
         slope_pair(:,plt) = eval([coll_calc '(diff(ps)./diff(C))']);%%EDIT mean instead of max
         clear bhat d_mean d_sd C ps
